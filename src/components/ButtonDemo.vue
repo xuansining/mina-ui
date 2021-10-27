3517c1e88fad77ebb1b2b13888dda7d73a0d2c4e
禤思宁 <5721834+xuanxuan_coding@user.noreply.gitee.com>
2021-10-27 21:39:09 +0800

    完成Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,7 +2,7 @@
   <div>Button 示例</div>
   <h1>示例1</h1>
   <div>
-    <Button>你好</Button>
+    <Button @click="onClick">你好</Button>
     <Button theme="button">你好</Button>
     <Button theme="link">你好</Button>
     <Button theme="text">你好</Button>
@@ -48,6 +48,12 @@
       <Button disabled>禁用按钮</Button>
       <Button theme="link" disabled>禁用链接按钮</Button>
       <Button theme="text" disabled>禁用按钮</Button>
+    </div>
+
+    <h1>示例5</h1>
+    <div>
+      <Button loading>加载中</Button>
+      <Button>加载完毕</Button>
     </div>
   </div>
 </template>
