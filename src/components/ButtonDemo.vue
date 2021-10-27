20ba6686f575ecd043101c84db96d1515b951f9f
禤思宁 <5721834+xuanxuan_coding@user.noreply.gitee.com>
2021-10-27 19:26:23 +0800

    将attrs绑定到button上

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,6 +1,25 @@
 <template>
-  <div>ButtonDemo的文档</div>
+  <div>
+    <h1>示例1</h1>
+    <Button @click="onClick" @focus="onClick" @mouseover="onClick" :size="size">
+      我是示例一
+    </Button>
+  </div>
 </template>
 <script lang="ts">
-export default {};
+import Button from "../libs/Button.vue";
+export default {
+  setup() {
+    const onClick = () => {
+      console.log("hi");
+    };
+    let size = "small";
+    return { onClick, size };
+  },
+  components: {
+    Button,
+  },
+};
 </script>
+<style lang="scss">
+</style>
