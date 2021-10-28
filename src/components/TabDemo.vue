e4b34939083e11f2eace23c8c94ef404f9d02c5a
禤思宁 <5721834+xuanxuan_coding@user.noreply.gitee.com>
2021-10-28 19:43:53 +0800

    实现下滑线移动

--- src/components/TabDemo.vue
+++ src/components/TabDemo.vue
@@ -1,18 +1,24 @@
 <template>
   <div>Tabs 示例</div>
   <h1>示例1</h1>
-  <Tabs>
+  <Tabs v-model:selected="x">
     <Tab title="导航1">内容1</Tab>
     <Tab title="导航2">内容2</Tab>
+    <Tab title="导航3">33333</Tab>
   </Tabs>
 </template>
 <script lang="ts">
 import Tabs from "../libs/Tabs.vue";
 import Tab from "../libs/Tab.vue";
+import { ref } from "vue";
 export default {
   components: {
     Tab,
     Tabs,
+  },
+  setup() {
+    const x = ref("导航1");
+    return { x };
   },
 };
 </script>
--- src/libs/Tabs.vue
+++ src/libs/Tabs.vue
@@ -1,25 +1,120 @@
 <template>
-  <div>
-    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-    <component v-for="(c, index) in defaults" :is="c" :key="index" />
+  <div class="mina-tabs">
+    <div class="mina-tabs-nav" ref="container">
+      <div
+        class="mina-tabs-nav-item"
+        v-for="(t, index) in titles"
+        :key="index"
+        @click="select(t)"
+        :class="{ selected: t === selected }"
+        :ref="
+          (el) => {
+            if (t === selected) selectedItem = el;
+          }
+        "
+      >
+        {{ t }}
+      </div>
+      <div class="mina-tabs-nav-indicator" ref="indicator"></div>
+    </div>
+    <div class="mina-tabs-content">
+      <component
+        class="mina-tabs-content-item"
+        v-for="(c, index) in defaults"
+        :is="c"
+        :key="index"
+        :class="{ selected: c.props.title == selected }"
+      />
+    </div>
   </div>
 </template>
 <script lang="ts">
+import { computed, ref, watchEffect, onMounted } from "vue";
 import Tab from "./Tab.vue";
 export default {
+  props: {
+    selected: {
+      type: String,
+    },
+  },
   setup(props, context) {
-    //获取子组件的所有对象
+    const selectedItem = ref<HTMLDivElement>(null);
+    const indicator = ref<HTMLDivElement>(null);
+    const container = ref<HTMLDivElement>(null);
+    onMounted(() => {
+      watchEffect(() => {
+        const { width } = selectedItem.value.getBoundingClientRect();
+        indicator.value.style.width = width + "px";
+        const { left: left1 } = container.value.getBoundingClientRect();
+        const { left: left2 } = selectedItem.value.getBoundingClientRect();
+        const left = left2 - left1;
+        indicator.value.style.left = left + "px";
+      });
+    });
     const defaults = context.slots.default();
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
-        throw new Error("Tab子标签必须是tab");
+        throw new Error("Tabs 子标签必须是 Tab");
       }
     });
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
-
-    return { titles, defaults };
+    const select = (title: string) => {
+      context.emit("update:selected", title);
+    };
+    return {
+      defaults,
+      titles,
+      select,
+      selectedItem,
+      indicator,
+      container,
+    };
   },
 };
 </script>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.mina-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    position: relative;
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms;
+    }
+
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item {
+      display: none;
+      &.selected {
+        display: block;
+      }
+    }
+  }
+}
+</style>
