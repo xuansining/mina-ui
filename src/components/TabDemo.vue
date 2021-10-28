5bf34b2e707ce7280d493d0bcdb181aa847ce321
禤思宁 <5721834+xuanxuan_coding@user.noreply.gitee.com>
2021-10-28 11:22:23 +0800

    成功创建Tabs组件

--- src/components/TabDemo.vue
+++ src/components/TabDemo.vue
@@ -1,6 +1,18 @@
 <template>
-  <div>TabDemo的文档</div>
+  <div>Tabs 示例</div>
+  <h1>示例1</h1>
+  <Tabs>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
 <script lang="ts">
-export default {};
+import Tabs from "../libs/Tabs.vue";
+import Tab from "../libs/Tab.vue";
+export default {
+  components: {
+    Tab,
+    Tabs,
+  },
+};
 </script>
---
+++ src/libs/Tabs.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
+    <component v-for="(c, index) in defaults" :is="c" :key="index" />
+  </div>
+</template>
+<script lang="ts">
+import Tab from "./Tab.vue";
+export default {
+  setup(props, context) {
+    //获取子组件的所有对象
+    const defaults = context.slots.default();
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error("Tab子标签必须是tab");
+      }
+    });
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+
+    return { titles, defaults };
+  },
+};
+</script>
